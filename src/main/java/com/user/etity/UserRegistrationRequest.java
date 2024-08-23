2f1043db311c55acdc69681276102f913181e5c0
Admin <sumanth.rinfo@gmail.com>
2024-08-23 15:53:28 -0700

    NEW CHANGES done neee

---
+++ src/main/java/com/user/etity/UserRegistrationRequest.java
@@ -0,0 +1,47 @@
+package com.user.etity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "USER")
+public class UserRegistrationRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+
+    public String name;
+
+
+    public UserRegistrationRequest() {
+    }
+
+    public UserRegistrationRequest(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRegistrationRequest{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
